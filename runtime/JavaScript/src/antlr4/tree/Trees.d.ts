aa3d6bdc0f078357004ab73c9e795b2025a8dcf0
Lawrence Zeng <lawrence.zeng@egmail.com>
2024-10-19 15:34:47 -0400

    remove private function

--- runtime/JavaScript/src/antlr4/tree/Trees.d.ts
+++ runtime/JavaScript/src/antlr4/tree/Trees.d.ts
@@ -7,6 +7,5 @@ declare namespace Trees {
     function findAllTokenNodes(t: any, ttype: any): any[];
     function findAllRuleNodes(t: any, ruleIndex: any): any[];
     function findAllNodes(t: any, index: any, findTokens: any): any[];
-    function _findAllNodes(t: any, index: any, findTokens: any, nodes: any): void;
     function descendants(t: any): any[];
 }
